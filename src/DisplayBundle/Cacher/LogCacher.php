0ad49c166145f7b28afd06fccae0ac0698db81a7
Roman Bovdui <roman.bovdui@toatech.com>
2015-10-15 11:33:16 +0300

    Cacher improved

--- src/DisplayBundle/Cacher/LogCacher.php
+++ src/DisplayBundle/Cacher/LogCacher.php
@@ -16,6 +16,8 @@ class LogCacher
     private $lineCollector = null;
     private $fileCollector = null;
 
+    private  $filesInCache = null;
+
     public function __construct(EntityManager $manager, LogFileCollector $fileCollector, LogLineCollector $lineCollector, $path, $refresh)
     {
         $this->path = $path;
@@ -25,25 +27,31 @@ class LogCacher
 
         $this->lineCollector = $lineCollector;
         $this->fileCollector = $fileCollector;
+        
+        $this->filesInCache = $this->em->getRepository('DisplayBundle:File');
     }
 
     public function refreshCache()
     {
-        if(!$this->isNeedRefresh()){echo 'no';
-            return true;
+        if(!$this->isNeedRefresh()){
+    //        return true;
         }
-echo 'yes';
+
         $path = str_replace('#username#', 'romanbvd', $this->path);
 
         $fileCollection = $this->fileCollector->getCollection($path);
         foreach($fileCollection as $file){
-            $this->em->persist($file);    
-            
+            if(!$this->isFileChanged($file)){
+               continue; 
+            }
+
+            $this->clearCacheForFile($file);
+
+            $this->saveFile($file); 
             $this->saveFileLines($file);
         }
 
         $this->em->flush();
-die;
        
         return 'hellos';
     }
@@ -63,6 +71,38 @@ die;
         }
 
         return false;
+    }
+
+    private function isFileChanged($file)
+    {
+        $foundFile = $this->filesInCache->findOneByName($file->getName());
+        
+        if(is_null($foundFile)){
+            return true;
+        }
+
+        if($foundFile->getDate() != $file->getDate()){
+          return true;
+        }
+
+        return false;
+    }
+
+    private function clearCacheForFile($file)
+    {
+        $foundFile = $this->filesInCache->findOneByName($file->getName());
+        
+        if(is_null($foundFile)){
+            return true;
+        }
+
+        $this->em->remove($foundFile);
+        $this->em->flush();
+    }
+
+    private function saveFile($fileObj)
+    {
+        $this->em->persist($fileObj);    
     }
 
     private function saveFileLines($fileObj)
