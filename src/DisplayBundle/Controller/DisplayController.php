fa6689329c4fa9b31561a90d78645a7048c4d708
roman <roman@debian.debian>
2015-10-17 21:34:26 +0300

    Paginator applied, data displayed on screen

--- src/DisplayBundle/Controller/DisplayController.php
+++ src/DisplayBundle/Controller/DisplayController.php
@@ -13,13 +13,21 @@ class DisplayController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $cache = $this->get('log_cacher');
-        
-    
         $cacher = $this->get('log_cacher');
         $cacher->refreshCache();
 
+        $em = $this->get('doctrine.orm.entity_manager');
+        $dql   = "SELECT l FROM DisplayBundle:Line l";
+        $query = $em->createQuery($dql);
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1)/*page number*/,
+            10/*limit per page*/
+        );
+
         return $this->render('DisplayBundle:Display:index.html.twig', array(
-            'var' => 's'));
+            'pagination' => $pagination));
     }
 }
