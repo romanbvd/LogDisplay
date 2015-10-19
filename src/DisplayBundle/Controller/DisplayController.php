ef673b7907ce6d0dfbd471a84b5dc4aaa6f5f260
Roman Bovdui <roman.bovdui@toatech.com>
2015-10-19 17:44:54 +0300

    Work on filters started

--- src/DisplayBundle/Controller/DisplayController.php
+++ src/DisplayBundle/Controller/DisplayController.php
@@ -16,6 +16,28 @@ class DisplayController extends Controller
         $cacher = $this->get('log_cacher');
         $cacher->refreshCache();
 
+        $form = $this->createFormBuilder()
+            ->add('user', 'text')
+            ->add('type', 'text')
+            ->add('availability', 'choice', array(
+                'choices' => array(
+                    'morning'   => 'Morning',
+                    'afternoon' => 'Afternoon',
+                    'evening'   => 'Evening',
+                ),
+                'multiple' => false,
+            ))
+            ->add('send', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            print_r($data);
+        }
+
         $em = $this->get('doctrine.orm.entity_manager');
         $dql   = "SELECT l FROM DisplayBundle:Line l";
         $query = $em->createQuery($dql);
@@ -27,7 +49,8 @@ class DisplayController extends Controller
             10/*limit per page*/
         );
 
-        return $this->render('DisplayBundle:Display:index.html.twig', array(
+        return $this->render('DisplayBundle:display:index.html.twig', array(
+            'search_form' => $form->createView(), 
             'pagination' => $pagination));
     }
 }
---
+++ src/DisplayBundle/Form/FormWorker.php
@@ -0,0 +1,13 @@
+<?php
+
+class FormWorker
+{
+    public function getFilter()
+    {
+    }
+
+    public function getQuery()
+    {
+
+    }
+}
