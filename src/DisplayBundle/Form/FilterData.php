5255be90cbf549eb86dab55e279bd4310f706382
roman <roman@debian.debian>
2015-10-21 23:51:00 +0300

    Manager added to FilterModel

--- src/DisplayBundle/Form/FilterData.php
+++ src/DisplayBundle/Form/FilterData.php
@@ -4,8 +4,34 @@ namespace DisplayBundle\Form;
 
 class FilterData
 {
-    public function getData()
+    public function __construct($mmm)
+    {
+        //print_r(get_class($mmm));
+    }
+    public function getUser()
     {
         return 'mmm';
     }
+
+    public function getType()
+    {
+        return 'lll';
+    }
+
+    public function getAvailability()
+    {
+        return array( 
+                'choices' => array(
+                    'morning'   => 'Morning',
+                    'afternoon' => 'Afternoon',
+                    'evening'   => 'Evening',
+            ));
+    }
+
+    public function setAvailability(ChoiceListInterface $choices)
+            {
+                        $this->choices = $choices;
+                                return $this;
+                            }
+
 }
